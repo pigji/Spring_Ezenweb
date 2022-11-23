ca6ae38279bfa66fab8ae48c91cdc9051240075e
pigji <pigji@naver.com>
2022-11-23 19:30:33 +0900

    11월23일수요일 비회원제게시판 만들기[ 글등록/출력, 카테고리 등록/출력, 삭제, 수정 --> 완료 ], 첨부파일 미완성

--- src/main/java/com/Ezenweb/controller/IndexController.java
+++ src/main/java/com/Ezenweb/controller/IndexController.java
@@ -25,6 +25,7 @@ public class IndexController {
     @Autowired
     private IndexlistService indexlistService ;
 
+
     // --------------------- 2. 페이지 요청( view )  -------------------- //
     // 1. 게시물 목록 페이지 열기
     @GetMapping("/list")    // http://localhost:8080/index/list
@@ -34,36 +35,38 @@ public class IndexController {
 
 
     // --------------------- 3. 요청과 응답 ( model ) ----------------------- //
-    // 1. 게시물 쓰기
-    @PostMapping("/setboard")
-    public boolean setboard( @RequestBody IndexDto indexDto ){
-        System.out.println( indexDto.toString() );
-        return indexlistService.setboard( indexDto );
-    }
 
-    // 2. 게시물 목록조회
-    @GetMapping("/boardlist")
-    public List<IndexDto> boardlist( @RequestParam("icno") int icno ){
-        return indexlistService.boardlist( icno );
-    }
-
-    // 3. 게시물 개별 조회
-    @GetMapping("/getboard")
-    public IndexDto getboard( @RequestParam("ino") int ino ){
-        return indexlistService.getboard( ino ); }
-
-    // 4. 카테고리 등록
+    // 1. 카테고리 등록
     @PostMapping("/setbcategory")
     public boolean setbcategory( @RequestBody IcategoryDto icategoryDto ){
         System.out.println( icategoryDto );
         return indexlistService.setbcategory( icategoryDto );
     }
 
-    // 5. 모든 카테고리 출력
+    // 2. 모든 카테고리 출력
     @GetMapping("/icategorylist")
     public List<IcategoryDto> icategorylist(){
         return indexlistService.icategorylist();
     }
+
+
+    // 3. 게시물 쓰기
+    @PostMapping("/setboard")
+    public boolean setboard( @RequestBody IndexDto indexDto ){
+        System.out.println( indexDto.toString() );
+        return indexlistService.setboard( indexDto );
+    }
+
+    // 4. 게시물 목록조회
+    @GetMapping("/boardlist")
+    public List<IndexDto> boardlist( @RequestParam("icno") int icno ){
+        return indexlistService.boardlist( icno );
+    }
+
+    // 5. 게시물 개별 조회
+    @GetMapping("/getboard")
+    public IndexDto getboard( @RequestParam("ino") int ino ){
+        return indexlistService.getboard( ino ); }
 
     // 6. 게시물 삭제
     @DeleteMapping("/delboard")
--- src/main/resources/static/js/index/list.js
+++ src/main/resources/static/js/index/list.js
@@ -4,37 +4,27 @@
 let icno = 0;   // 선택된 카테고리 , 기본값 0 : 전체보기
 
 // 1. 세션스토리지 호출
-let ino = sessionStorage.getItem("ino");
+let ino = 0;
 
-
-
-// 1. 게시물 출력
-boardlist()
-function boardlist(){
+// ---------------- 1. 카테고리 등록버튼을 눌렀을때  ------------------- //
+function setbcategory(){
+    let data = { icname : document.querySelector(".icname").value }
     $.ajax({
-        url: "/index/boardlist",    // Controller 응답, 요청 주소
-        type: "GET",
-        data: { "icno" : icno },    // icno : 카테고리 번호
-        success: function( list ){
-            console.log( list )
-
-            let html = '<tr> <th> 번호 </th> <th> 제목 </th> <th> 작성자 </th></tr>';
-            list.forEach( (b) => {
-                  html +=
-                       '<tr> <td> '+b.ino+' </td> <td onclick="getview('+b.ino+')">'+b.ititle+' </td> '+
-                       ' <td><button type="button" onclick="upboard('+b.ino+')"> 수정 </button></td> ' +
-                      ' <td><button type="button" onclick="delboard('+b.ino+')"> 삭제 </button></td></tr>';
-            })
-            document.querySelector(".btable").innerHTML = html;
+        url: "/index/setbcategory",
+        type: "POST",
+        data : JSON.stringify(data),
+        contentType: "application/json",
+        success : function( re ){
+            alert( re )
+            if( re == true ){
+                alert("카테고리 등록 성공!!");
+                icategorylist();
+            }else{ alert("카테고리 등록 실패..") }
         }
     })
 }
 
-
-
-
-
-// 3. 모든 카테고리 출력
+// ---------------- 2. 모든 카테고리 출력 ------------------- //
 icategorylist()
 function icategorylist(){
     $.ajax({
@@ -53,13 +43,13 @@ function icategorylist(){
     })
 }
 
-// 4. 카테고리를 선택했을때 번호 대입
+// ---------------- 2-2. 카테고리를 선택했을때 번호 대입 ------------------- //
 function bcnochage( ino ){
     icno = ino; alert( icno+"번 카테고리 선택");
     boardlist();
 }
 
-// 1. 게시물등록 버튼을 눌렀을때
+// ---------------- 3. 게시물 등록버튼을 눌렀을때  ------------------- //
 function setboard(){
     let data = {
         iname : document.querySelector('.iname').value,     // 작성자
@@ -87,33 +77,30 @@ function setboard(){
     })
 }
 
-// 2. 카테고리 등록버튼 눌렀을때
-function setbcategory(){
-    let data = { icname : document.querySelector(".icname").value }
+// ---------------- 4. 게시물 출력 ------------------- //
+boardlist()
+function boardlist(){
     $.ajax({
-        url: "/index/setbcategory",
-        type: "POST",
-        data : JSON.stringify(data),
-        contentType: "application/json",
-        success : function( re ){
-            alert( re )
-            if( re == true ){
-                alert("카테고리 등록 성공!!");
-                icategorylist();
-            }else{ alert("카테고리 등록 실패..") }
+        url: "/index/boardlist",    // Controller 응답, 요청 주소
+        type: "GET",
+        data: { "icno" : icno },    // icno : 카테고리 번호
+        success: function( list ){
+            console.log( list )
+
+            let html = '<tr> <th> 번호 </th> <th> 제목 </th> <th> 작성자 </th></tr>';
+            list.forEach( (b) => {
+                  html +=
+                       '<tr> <td> '+b.ino+' </td> <td onclick="getview('+b.ino+')">'+b.ititle+' </td> '+
+                       ' <td><button type="button" onclick="upboard('+b.ino+')"> 수정 </button></td> ' +
+                      ' <td><button type="button" onclick="delboard('+b.ino+')"> 삭제 </button></td></tr>';
+            })
+            document.querySelector(".btable").innerHTML = html;
         }
     })
 }
 
 
-
-// 1. list.js 에서 클릭한 게시물번호 호출
-//let ino = sessionStorage.getItem("ino");
-//    alert( ino+"번 게시물") // 확인
-
-
-
-// 4. 게시물 삭제버튼을 눌렀을때
+// ---------------- 5. 게시물 삭제버튼을 눌렀을때 ------------------- //
 function delboard(ino){
     $.ajax({
         url : "/index/delboard",
@@ -130,7 +117,7 @@ function delboard(ino){
     })
 }
 
-// 2. 수정 전 게시물정보 호출 [ 개별 게시물 호출 ]
+// ---------------- 6. 게시물 수정 전 내용 호출 ------------------- //
 getboard()
 function getboard(){
     $.ajax({
@@ -141,39 +128,70 @@ function getboard(){
     })
 }
 
-// 3. 수정하기 버튼을 눌렀을때
-function upboard(){
-    let data = {
-        ititle : document.querySelector('.ititle').value,
-        icontent : document.querySelector('.icontent').value,
-        ifile : document.querySelector('.ifile').value,
-        ino : ino
-    }
+
+// ---------------- 7. 수정버튼을 눌렀을때 ------------------- //
+// 수정하기 버튼 눌렀을때 수정창 열림
+function upboard( no ){
+    ino = no;
+    alert( ino+"번 게시물 수정 클릭" )
 
     $.ajax({
-        url : "/index/upboard",
-        type : "put",
-        data : JSON.stringify(data),
-        contentType: "application/json",
-        success : function( re ) {
-            console.log( re );
+        url : "/index/getboard",
+        type : "get",
+        data : { "ino" : ino },
+        success : function(re) {
 
-            let html = '<h3> 게시물 수정페이지 '+
-                        '</h3> <tr> <th> 번호 </th> <th> 제목 </th> <th> 작성자 </th></tr>';
-                        list.forEach( (d) => {
-                              html +=
-                                   '<tr> <td> '+b.ino+' </td> <td onclick="getview('+b.ino+')">'+b.ititle+' </td> '+
-                                   ' <td><button type="button" onclick="upboard('+b.ino+')"> 수정 </button></td> ' +
-                                  ' <td><button type="button" onclick="delboard('+b.ino+')"> 삭제 </button></td></tr>';
-                        })
-                        document.querySelector(".btable").innerHTML = html;
+             let html2 =  '<h3> 수정페이지 </h3>'+
+                            '제목 : <input type="text" class="upititle">'+
+                            '내용 : <input type="text" class="upicontent">'+
+                            '파일 : <input type="text" class="upifile">'+
+                            '<button type="button" onclick="upupboard()"> 수정 </button>';
+
+                            console.log(html2)
+                            document.querySelector('.up').innerHTML = html2;
+
+                            // 수정한 내용 저장
+                            document.querySelector('.upititle').value = re.ititle
+                             document.querySelector('.upicontent').value = re.icontent
+                              document.querySelector('.upifile').value = re.ifile
 
 
-            if( re == true ){
-                alert("글수정 성공!!");
-                boardlist()
-            }else{ alert("글수정 실패.."); }
-        }
+         }
     })
 }
+// 수정 내용 입력후 수정완료 버튼을 눌렀을때
+function upupboard( ){
+
+        let data = {
+            ititle : document.querySelector('.upititle').value,
+            icontent : document.querySelector('.upicontent').value,
+            ifile : document.querySelector('.upifile').value,
+            ino : ino
+        }
+
+            $.ajax({
+                url : "/index/upboard",
+                type : "put",
+                data : JSON.stringify(data),
+                contentType: "application/json",
+                success : function( re ) {
+                    if( re == true ){
+                        alert("글수정 성공!!");
+                        boardlist()
+                    }else{ alert("글수정 실패.."); }
+                }
+            })
+
+}
+/*
+
+         수정 js
+        1. 수정버튼을 클릭한다.
+        2. 클릭한 게시물의 번호를 저장한다,
+        3. 저장된 게시물의 번호를 가지고 수정할 페이지에 게시물정보 대입한다.
+        4. 수정버튼을 클릭했을때 게시물번호+수정할내용 ajax 컨트롤에게 보낸다.
+
+
+*/
+
 
